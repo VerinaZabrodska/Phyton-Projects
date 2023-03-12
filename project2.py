5899d043b98a3326fc230c62dfae73e61edc569f
Verina <108272750+VerinaZabrodska@users.noreply.github.com>
2023-03-12 18:21:46 +0200

    Add files via upload

---
+++ project2.py
@@ -0,0 +1,5 @@
+print('What is your name?')
+s1 = input()
+print("Hello, " + " " + s1)
+
+
---
+++ project5.py
@@ -0,0 +1,12 @@
+a = int(input('Число 1:'))
+b = int(input('Число 2:'))
+c = 1
+
+while a < b:
+    a+=c
+    print('Тепер число 1 =', a)
+
+if a>=b:
+    print('Фініш!')
+
+
